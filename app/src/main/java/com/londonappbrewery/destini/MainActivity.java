3888f88ba8a63ec848f7d50e96edf109c8995b4f
Brandon Gailey <brandon.gailey7@gmail.com>
2017-09-29 12:38:33 -0400

    Updated code

--- app/src/main/java/com/londonappbrewery/destini/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/destini/MainActivity.java
@@ -2,11 +2,15 @@ package com.londonappbrewery.destini;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     // TODO: Steps 4 & 8 - Declare member variables here:
-
+    Button topButton;
+    Button bottomButton;
+    TextView storyView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
